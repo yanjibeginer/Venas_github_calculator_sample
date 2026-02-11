7737c6e55d8c8fac6af5b7b2c021c541332f8e92
Diestadotcom <nickdiesta1@gmail.com>
2026-02-11 20:27:59 +0800

    feat(add substraction)

---
+++ Venas_github_calculator_sample/Substraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venas_github_calculator_sample
+{
+    public class Substraction 
+    {
+
+        public double substraction(double num1, double num2)
+        {
+            return  num1 - num2;
+        
+        }
+ 
+
+
+}
+}
