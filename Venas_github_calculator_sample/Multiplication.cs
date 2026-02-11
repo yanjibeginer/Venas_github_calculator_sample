0d10eab3451a29f9984fdef733b581e35c32b623
pachecodotcom <jbpacheco000@gmail.com>
2026-02-11 20:27:56 +0800

    feat(add Multiplication)

---
+++ Venas_github_calculator_sample/Multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venas_github_calculator_sample
+{
+    public class Multiplication
+    {
+        public double multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+       
+    }
+
+}
